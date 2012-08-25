3ed3f20cf9621df71c0e82ba2db261bcfc2b5942
Tristan Hardy <tristan@tristanhardy.com>
2012-08-25 18:59:22 -0400

    homework section 1 complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+before_filter :ensure_products_present 
+ def index
+    @products=Product.includes(:user).all
+    
+  end
+  def ensure_products_present
+    redirect_to "/" unless Product.count>0
+  end
 end
